653a1cdf3d1f5014383a8aa3c215b7168ef6681d
Kaoutar <kaoutarmghar@gmail.com>
2022-05-09 15:50:51 +0000

    version6

---
+++ src/app/view/postuler/information-sur-demandeur/information-sur-demandeur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-information-sur-demandeur',
+  templateUrl: './information-sur-demandeur.component.html',
+  styleUrls: ['./information-sur-demandeur.component.css']
+})
+export class InformationSurDemandeurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
